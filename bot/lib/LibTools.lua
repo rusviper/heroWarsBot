e7975f61ec77e45c228a434f0575380ed5eb3d1c
rusviper <rusviper@gmail.com>
2024-08-03 01:11:19 +0400

    comment

--- bot/lib/LibTools.lua
+++ bot/lib/LibTools.lua
@@ -182,7 +182,7 @@ function LibTools:waitOneOf(picTable, timeout, step)
     for i = 0, timeout, step do
         found = LibTools:findFirstOf(picTable)
         if (found ~= nil) then
-            Txt:toast("Найдено! " .. found .. " изображение за " .. (i * step) .. " сек.")
+            Txt:toast("Найдено! " .. found:typeOf() .. " изображение за " .. (i * step) .. " сек.")
             return found
         end
     end
